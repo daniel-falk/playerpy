e7ca20668bc185a8ad87827376e192aa1b4e6995
Kelli Davis <keldavis5@yahoo.com>
2021-07-18 12:09:26 -0400

    Displayed image and added mouse and key listener (#2)
    * Displayed image and added mouse and key listener
    
    * Displayed image and added mouse and key listener

--- web/server.py
+++ web/server.py
@@ -2,11 +2,12 @@ from simple_websocket_server import WebSocketServer, WebSocket
 import threading
 import time
 
+#Images path
 IMAGES = [
-        "https://pngimg.com/uploads/number0/number0_PNG19165.png",
-        "https://upload.wikimedia.org/wikipedia/commons/8/81/Linea_1.png",
-        "https://upload.wikimedia.org/wikipedia/commons/9/9f/Icon_2_%28set_basic%29.png",
-        "https://upload.wikimedia.org/wikipedia/commons/2/26/MRT_Singapore_Destination_3.png"
+    "https://pngimg.com/uploads/number0/number0_PNG19165.png",
+    "https://upload.wikimedia.org/wikipedia/commons/8/81/Linea_1.png",
+    "https://upload.wikimedia.org/wikipedia/commons/9/9f/Icon_2_%28set_basic%29.png",
+    "https://upload.wikimedia.org/wikipedia/commons/2/26/MRT_Singapore_Destination_3.png"
 ]
 
 
@@ -16,37 +17,55 @@ class SimpleImageSource(WebSocket):
         super().__init__(*args, **kwargs)
 
     def send_image_path(self):
+        """
+        Send image path
+        """
         img_path = IMAGES[self.idx % len(IMAGES)]
-        self.send_message("img:" + img_path)
+        self.send_message(img_path)
 
     def handle(self):
-        # Receive an event from the front-end and act on it
+        """
+        Receive an event from the front-end and act on it
+        """
         print("Event: %s" % self.data)
-        if self.data == "ARROW_LEFT":
-            self.idx -= 1
-        elif self.data == "ARROW_RIGHT":
-            self.idx += 1
+        if self.data.split('=')[0] == "EVENT(key)":
+            if self.data.split('=')[1] == "ArrowLeft":
+                self.idx -= 1
+            elif self.data.split('=')[1] == "ArrowRight":
+                self.idx += 1
+            else:
+                return
+        elif self.data.split('=')[0] == "COORDINATES(x,y)":
+            return
         else:
             print("Unknown event: %s" % str(self.data))
             return
         self.send_image_path()
 
     def connected(self):
+        """
+        Display connected address and send image path
+        """
         print(self.address, 'connected')
         self.send_image_path()
 
     def handle_close(self):
+        """
+        Display closed address
+        """
         print(self.address, 'closed')
 
 
 def advance_loop(server):
-    """Advance every image source one frame per 3 seconds
+    """
+    Advance every image source one frame per 3 seconds
     """
     while True:
         for _, image_source in server.connections.items():
             image_source.idx += 1
             image_source.send_image_path()
         time.sleep(10)
+
 
 server = WebSocketServer('', 8000, SimpleImageSource)
 th = threading.Thread(target=advance_loop, args=(server,), daemon=True)
